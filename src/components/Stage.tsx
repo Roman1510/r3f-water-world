65b4fbbd55b5628c0dc0280cb1dbdd979d581821
Roman1510 <romanvinnick@gmail.com>
2024-06-16 14:45:39 +0200

    adding seaweed

--- src/components/Stage.tsx
+++ src/components/Stage.tsx
@@ -1,30 +1,45 @@
+import { useMemo } from 'react'
+import { useGLTF } from '@react-three/drei'
 import { Player } from './Player'
 import { WaterPlane } from './WaterPlane'
 import { Vector3 } from 'three'
 
 export function Stage() {
-  const numBoxes = 1000
-  const planeScale = 2000
+  const numSeaweeds = 2000
+  const planeScale = 4000
 
-  // Generate random floating boxes positions
-  const floatingBoxesPositions = Array.from({ length: numBoxes }, () => {
-    const x = (Math.random() - 0.5) * planeScale
-    const z = (Math.random() - 0.5) * planeScale
-    const y = Math.random() * 1 - 8 // Adjust Y range as needed
-    return new Vector3(x, y, z) // Convert to Vector3 object
-  })
+  // Generate random floating seaweed positions
+  const seaweedPositions = useMemo(() => {
+    return Array.from({ length: numSeaweeds }, () => {
+      const x = (Math.random() - 0.5) * planeScale
+      const z = (Math.random() - 0.5) * planeScale
+      const y = Math.random() * 1 - 25.5 // Adjust Y range as needed
+      return new Vector3(x, y, z)
+    })
+  }, [numSeaweeds, planeScale])
+
+  // Load the seaweed model
+  const { scene: seaweedModel } = useGLTF('/new-seaweed.glb')
+
+  // Apply scaling
+  seaweedModel.scale.set(5, 5, 5)
 
   return (
     <>
       <Player />
       <WaterPlane />
 
-      {floatingBoxesPositions.map((position, index) => (
-        <mesh position={position} key={index}>
-          <boxGeometry args={[1, 1, 1]} />
-          <meshStandardMaterial color="darkgrey" />
-        </mesh>
+      {seaweedPositions.map((position, index) => (
+        <primitive
+          object={seaweedModel.clone()}
+          position={position}
+          key={index}
+        />
       ))}
     </>
   )
 }
+
+// This line is needed to avoid warnings about missing GLTF loader.
+// If you have a custom loader, replace the path with your loader's path.
+useGLTF.preload('/new-seaweed.glb')
